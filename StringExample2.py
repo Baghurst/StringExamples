7af9b5bac54214b7c5d5cde5c225578c5361bed0
Baghurst <23bbaghurst.acc@lwetb.ie>
2024-11-18 18:49:22 +0000

    Add files via upload

---
+++ StringExample2.py
@@ -0,0 +1,27 @@
+#Example1: string.capitalize()
+#Returns a copy of the string with its first character capitalized.
+
+"true".capitalize() 
+"i love my India.".capitalize()
+
+#Example2:
+
+string = "it goes as - ringa ringa roses"
+sub = "ringa"
+string.find(sub)
+string.find(sub, 15, 25)
+string.find(sub, 15)
+"string".isalnum()
+
+
+
+
+
+
+
+
+
+
+
+
+
