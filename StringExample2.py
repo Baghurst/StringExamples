71facea36f64f7714b1566ea4ad14ec237eb4c81
Baghurst <23bbaghurst.acc@lwetb.ie>
2024-11-18 19:13:24 +0000

    Update StringExample2.py

--- StringExample2.py
+++ StringExample2.py
@@ -1,17 +1,20 @@
 #Example1: string.capitalize()
 #Returns a copy of the string with its first character capitalized.
 
-"true".capitalize() 
-"i love my India.".capitalize()
+"true".capitalize() #Turns the string "true" in capital, turning it in "True".
+"i love my India.".capitalize() #The same thing.
 
-#Example2:
+#Example2: string.find(sub[, start[, end]])
+#Returns the lowest index in the string where the substring sub is found within the slice range of start and end. Returns 1 if sub is not found.
 
-string = "it goes as - ringa ringa roses"
+string = "it goes as - ringa ringa roses" 
 sub = "ringa"
 string.find(sub)
 string.find(sub, 15, 25)
 string.find(sub, 15)
 "string".isalnum()
+
+#Example3:
 
 
 
