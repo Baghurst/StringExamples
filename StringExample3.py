1743f5038224948910da5eb127ad5daefb40f519
Baghurst <23bbaghurst.acc@lwetb.ie>
2024-11-21 12:02:06 +0000

    Add files via upload

---
+++ StringExample3.py
@@ -0,0 +1,37 @@
+#STRING MANIPULATION: CHAPTER 9: Pag.287
+#5. Write a program that takes a string with multiple words and then capitalizes the first letter of each word and forms a new string out of it.
+string = input("Enter a string: ")
+lenght = len(string)                                                                                                                                                                                                 
+for a 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
