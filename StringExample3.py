06582082679fa1ebd869c8f81502b931ff94f174
Baghurst <23bbaghurst.acc@lwetb.ie>
2024-11-25 13:40:16 +0000

    Update StringExample3.py

--- StringExample3.py
+++ StringExample3.py
@@ -1,14 +1,14 @@
 #STRING MANIPULATION: CHAPTER 9: Pag.287
 #5. Write a program that takes a string with multiple words and then capitalizes the first letter of each word and forms a new string out of it.
-string = input("Enter a string: ")
-length = len(string)
-result = ""
+string = input("Enter a string: ") 
+length = len(string) #(lenght) = the lenght of (string)
+result = "" #
 a = 0
 while a < length:
     if a == 0:  
         result += string[a].upper()
         a += 1
-    elif string[a] == ' ' and a + 1 < length and string[a + 1] != ' ': 
+    elif string[a] == " " and a + 1 < length and string[a + 1] != " ": 
         result += string[a]  
         result += string[a + 1].upper() 
         a += 2  
@@ -18,32 +18,10 @@ while a < length:
 print("Original String:", string)
 print("Capitalized Words String:", result)
 
+#6. Write a program that reads a string and checks whether it is a palidrome string or not. (Example, noon and racecar are the same either way)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+string = input("Enter a string: ")
+length = len(string)
+mid = lenght // 2 
+rev = -1
 
